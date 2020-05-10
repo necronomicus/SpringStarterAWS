450cb2ae13f1b3debee407d4b61eeeb0e1249cec
Juan Pablo Bustos Paulich <juan.bustos@globant.com>
2020-05-09 21:03:51 -0300

    Initial Commit

---
+++ src/main/java/springboot/app/StarterProyectAwsApplication.java
@@ -0,0 +1,13 @@
+package springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterProyectAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterProyectAwsApplication.class, args);
+	}
+
+}
