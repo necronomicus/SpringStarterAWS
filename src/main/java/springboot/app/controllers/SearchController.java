ca48030e43c50630244a966604d4a320c3e50e28
Juan Pablo Bustos Paulich <juanpaulich2020@gmail.com>
2020-05-17 21:04:22 -0300

    Pushing new Resources

--- src/main/java/springboot/app/controllers/SearchController.java
+++ src/main/java/springboot/app/controllers/SearchController.java
@@ -1,8 +1,27 @@
 package springboot.app.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import springboot.app.models.Raza;
+import springboot.app.service.SBuscarRaza;
 
 @Controller
 public class SearchController {
+	
+	@Autowired
+	private SBuscarRaza sbr;
+	
+	@GetMapping(value = "/Razas", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public List<Raza> getAll()
+	{
+		return sbr.getAll();
+	}
 
 }
--- src/main/java/springboot/app/service/SBuscarRaza.java
+++ src/main/java/springboot/app/service/SBuscarRaza.java
@@ -1,5 +1,45 @@
 package springboot.app.service;
 
-public class SBuscarRaza {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+
+import springboot.app.models.Raza;
+
+@Service
+public class SBuscarRaza {
+	
+	public List<Raza> getAll(){
+		
+		Regions region = Regions.SA_EAST_1; 
+		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+				.withRegion(region)
+				.build();
+		String tableName = "Razas";
+		DynamoDB dynamoDB = new DynamoDB(client);
+		Table table = dynamoDB.getTable(tableName);
+		ScanRequest scan = new ScanRequest().withTableName(tableName); 
+		ScanResult result = client.scan(scan);
+		List<Raza> list = new ArrayList<Raza>();
+		for(Map<String, AttributeValue> item : result.getItems())//ESTO ES UN LISTA DE MAPAS
+		{
+			Raza r = new Raza();
+			r.setId(Integer.parseInt(item.get("id").getN()));
+			r.setRaza(item.get("raza").getS());
+			r.setMisc(item.get("misc").getS());
+			list.add(r);
+		}
+		return list;
+	}
 }
